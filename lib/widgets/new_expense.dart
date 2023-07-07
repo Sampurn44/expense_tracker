858c2bdcc3a0291f89267909601784206cf60ff9
Sampurn Chouksey <sampurn10chouksey@gmail.com>
2023-07-08 03:47:58 +0530

    "Modal Button added and cancel button working"

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
+              label: Text("Title"),
+            ),
+          ),
+          const SizedBox(height: 10),
+          TextField(
+            controller: _amountController,
+            maxLength: 10,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '₹ ',
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text("SAVE"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
